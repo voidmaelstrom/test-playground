2d4c8ccdaf3537c464333aa87433ee33357aa0cc
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-04-04 19:57:25 -0600

    Setup app and tests for button and checkbox

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,49 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react"
+import App from './App'
+ import userEvent from '@testing-library/user-event'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("header renders with correct text", () => {
+  render(<App />)
+  const headerEl = screen.getByRole("heading")
+  expect(headerEl.textContent).toBe("Testing Playground")
+})
+
+describe('test for the button and checkbox', () =>{
+  test("button changes color when clicked", () => {
+    render(<App />)
+    const colorBtn = screen.getByRole("button")
+  
+    userEvent.click(colorBtn)
+  
+    expect(colorBtn).toHaveStyle({ backgroundColor: "blue" })
+    expect(colorBtn.textContent).toBe("Change button color to green")
+  })
+
+  test("checkbox disables button when first clicked, then enables button on second click", () => {
+    render(<App />)
+    const colorBtn = screen.getByRole("button")
+    const checkbox = screen.getByRole("checkbox")
+  
+    userEvent.click(checkbox)
+    expect(checkbox).toBeChecked()
+    expect(colorBtn).toBeDisabled()
+  
+    userEvent.click(checkbox)
+    expect(checkbox).toHaveProperty('checked', false)
+    expect(colorBtn).toBeEnabled()
+  })
+
+  test("accurate text appears when button is enabled or disabled", () => {
+    render(<App />)
+    const checkbox = screen.getByRole("checkbox")
+    const paragraphEl = screen.getByRole("paragraph")
+  
+    expect(paragraphEl.textContent).toBe("Button is enabled")
+  
+    userEvent.click(checkbox)
+    expect(paragraphEl.textContent).toBe("Button is disabled")
+  
+    userEvent.click(checkbox)
+    expect(paragraphEl.textContent).toBe("Button is enabled")
+  })
+ })
